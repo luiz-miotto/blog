e8e23109cdbe867f90af9a6cda42906f9e4a983d
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2023-11-05 13:48:30 -0500

    committing before refactoring to use partial to display existing comments on articles

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find_by(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+end
